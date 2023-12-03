16110327d56f1a35709224912d07f85f6d380de0
Alexander Kies <kies@fias.uni-frankfurt.de>
2023-12-03 06:51:56 +0100

    Create network_setup.py

---
+++ src/network_setup.py
@@ -0,0 +1,6 @@
+def setup_pypsa_model(weather_data):
+    # Initialize and configure the PyPSA model with the given weather data
+    network = pypsa.Network()
+
+    # Return the configured network
+    return network
